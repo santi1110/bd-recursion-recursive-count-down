ae5827f5ae055c77eba64c25cdf634e44c6a9446
Matt McCarley <mdmccarley89@gmail.com>
2021-11-02 11:36:07 -0700

    Add initial files

---
+++ Solution/RecursiveCountDown.java
@@ -0,0 +1,17 @@
+package com.amazon.ata.recursion.prework;
+
+public class RecursiveCountDown {
+
+    /**
+     * This recursive method will return a string consisting of all the integers from the target
+     * down to zero. ex: target=3 will return "3210"
+     * @param target - this is the number we are counting down from
+     * @return - this is a String that has combined all the numbers from target down to 0.
+     */
+    public String countDown(int target) {
+        if (target <= 0) {
+            return "0";
+        }
+        return target + countDown(target - 1);
+    }
+}
